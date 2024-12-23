3c8a5f40e827017623dcba737750a3c5fff88fd5
Shoiya Akari <akariondev@gmail.com>
2024-12-23 14:32:43 -0300

    moe.sh: adapt build for fogos

--- moe.sh
+++ moe.sh
@@ -4,65 +4,47 @@
 # Copyright (C) 2024 Shoiya A.
 
 SECONDS=0
-PATH=$PWD/toolchain/bin:$PATH
+CLANG_VERSION="clang-17.0.0"
+TC_DIR="$HOME/tc/$CLANG_VERSION"
+PATH=$HOME/tc/$CLANG_VERSION/bin:$PATH
 export modpath=AnyKernel3/modules/vendor/lib/modules
 export ARCH=arm64
 export KBUILD_BUILD_USER=Moe
 export KBUILD_BUILD_HOST=Nyan
-
-export LLVM_DIR=$PWD/toolchain/bin
-#export LLVM_DIR=$HOME/tc/clang-20.0.0/bin
-#TC_DIR="$HOME/tc/clang-20.0.0"
-#export PATH="$TC_DIR/bin:$PATH"
+export LLVM_DIR=$HOME/tc/$CLANG_VERSION/bin
 export LLVM=1
 
 AK3_DIR="$HOME/AnyKernel3"
-DEFCONFIG="vendor/fogos_defconfig"
-ZIPNAME="MoeKernel-fogos-$(date '+%Y%m%d-%H%M').zip"
+VARIANTS=("fogos" "fogos")
+DEFCONFIGS=("vendor/fogos_defconfig" "vendor/fogos_defconfig")
+ZIPNAME_PREFIX="MoeKernel-$(date '+%Y%m%d-%H%M')"
+LOG_FILE="moe.log"
+: > "$LOG_FILE"
 
-url_init_clang="https://github.com/MoeKernel/scripts/raw/ksu/init_clang.sh"
-file_init_clang="$PWD/init_clang.sh"
-
-owo() {
-    local url="$1"
-    local file="$2"
-
-    if [ ! -f "$file" ]; then
-        echo "File $file not found. Downloading..."
-        wget "$url" -O "$file"
-        if [ $? -eq 0 ]; then
-            echo "Download of $file completed."
-            chmod +x "$file"
-            echo "Execute permissions added to $file."
-        else
-            echo "Failed to download $file."
-            return 1
-        fi
-    else
-        echo "File $file already exists."
-    fi
-
-    echo "Executing $file..."
-    "$file"
-    if [ $? -eq 0 ]; then
-        echo "$file executed successfully."
-    else
-        echo "Failed to execute $file."
-    fi
-}
-
-owo "$url_init_clang" "$file_init_clang"
-
-if [[ $1 = "-m" || $1 = "--menu" ]]; then
-    mkdir -p out
-    make O=out ARCH=arm64 $DEFCONFIG menuconfig
-elif [[ $1 = "menu" ]]; then
-    mkdir -p out
-    make O=out ARCH=arm64 $DEFCONFIG menuconfig
-else
-    mkdir -p out
-    make O=out ARCH=arm64 $DEFCONFIG
+if [[ $# -ne 2 || $1 != "--variant" || ! " ${VARIANTS[@]} " =~ " $2 " ]]; then
+    echo "Use: $0 --variant {fogos}" | tee -a "$LOG_FILE"
+    exit 1
 fi
+
+VARIANT="$2"
+if [[ "$VARIANT" == "fogos" ]]; then
+    DEFCONFIG="${DEFCONFIGS[0]}"
+elif [[ "$VARIANT" == "fogos" ]]; then
+    DEFCONFIG="${DEFCONFIGS[1]}"
+fi
+
+if ! [ -d "${TC_DIR}" ]; then
+    echo "Clang not found! Cloning to ${TC_DIR}..." | tee -a "$LOG_FILE"
+    if ! git clone --depth=1 https://gitlab.com/moehacker/clang-r487747.git ${TC_DIR} >> "$LOG_FILE" 2>&1; then
+        echo "Cloning failed! Aborting..." | tee -a "$LOG_FILE"
+        exit 1
+    fi
+fi
+
+echo -e "\nCompiling for $DEFCONFIG with variant $VARIANT..." | tee -a "$LOG_FILE"
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG | tee -a "$LOG_FILE"
 
 ARGS='
 CC=clang
@@ -82,30 +64,30 @@ LLVM_NM='${LLVM_DIR}/llvm-nm'
 LLVM=1
 '
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make $ARGS $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
-	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
-	exit
+make ${ARGS} O=out $DEFCONFIG moto.config | tee -a "$LOG_FILE"
+make ${ARGS} O=out -j$(nproc) | tee -a "$LOG_FILE"
+
+if [ ! -e "out/arch/arm64/boot/Image" ]; then
+    echo "ERROR: Image binary not found. Compilation failed!" | tee -a "$LOG_FILE"
+    exit 1
 fi
 
-make ${ARGS} O=out $DEFCONFIG moto.config
-make ${ARGS} O=out -j$(nproc)
-
-[ ! -e "out/arch/arm64/boot/Image" ] && \
-echo "  ERROR : image binary not found in any of the specified locations , fix compile!" && \
-exit 1
-
-make O=out ${ARGS} -j$(nproc) INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
+make O=out ${ARGS} -j$(nproc) INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install | tee -a "$LOG_FILE"
+echo -e "\nKernel compiled successfully for $DEFCONFIG! Zipping up...\n" | tee -a "$LOG_FILE"
 
 if [ -d "$AK3_DIR" ]; then
-	cp -r $AK3_DIR AnyKernel3
-	git -C AnyKernel3 checkout fogos_ksu &> /dev/null
-elif ! git clone -q https://github.com/MoeKernel/AnyKernel3 -b fogos_ksu; then
-	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
-	exit 1
+    cp -r $AK3_DIR AnyKernel3
+    git -C AnyKernel3 checkout fogos_ksu &> /dev/null
+else
+    git clone -q https://github.com/MoeKernel/AnyKernel3 -b fogos_ksu
 fi
+
+cp out/.config AnyKernel3/config
+cp out/arch/arm64/boot/Image AnyKernel3/Image
+cp out/arch/arm64/boot/dtb.img AnyKernel3/dtb
+cp out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
+
+ZIPNAME="${ZIPNAME_PREFIX}-${VARIANT}.zip"
 
 mkdir -p ${modpath}
 kver=$(make kernelversion)
@@ -113,24 +95,26 @@ kmod=$(echo ${kver} | awk -F'.' '{print $3}')
 mkdir -p AnyKernel3/modules/vendor/lib/modules 
 kver=$(make kernelversion)
 kmod=$(echo ${kver} | awk -F'.' '{print $3}')
+
 cp out/.config AnyKernel3/config
 cp out/arch/arm64/boot/Image AnyKernel3/Image
 cp out/arch/arm64/boot/dtb.img AnyKernel3/dtb
 cp out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
-# cp build.sta/${DEVICE}_modules.blocklist ${modpath}/modules.blocklist
 cp $(find out/modules/lib/modules/5.4* -name '*.ko') ${modpath}/
 cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} ${modpath}/
 cp out/modules/lib/modules/5.4*/modules.order ${modpath}/modules.load
+
 sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' ${modpath}/modules.dep
 sed -i 's/.*\///; s/\.ko$//' ${modpath}/modules.load
-source build.sta/${DEVICE}_mdconf
+
 for useles_modules in "${modules_to_nuke[@]}"; do
   grep -vE "$useles_modules" ${modpath}/modules.load > /tmp/templd && mv /tmp/templd ${modpath}/modules.load
 done
+
 cd AnyKernel3
-zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder | tee -a "../$LOG_FILE"
 cd ..
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo "Zip: $ZIPNAME"
-#curl -F "file=@$ZIPNAME" https://temp.sh/upload
+
+echo -e "\nCompleted compilation for $DEFCONFIG (variant $VARIANT) in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!" | tee -a "$LOG_FILE"
+echo "Zip: $ZIPNAME" | tee -a "$LOG_FILE"
 rm -rf AnyKernel3
