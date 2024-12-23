2855447a563b06d9fe39408a6fb1917496640910
Shoiya Akari <akariondev@gmail.com>
2024-12-23 14:35:50 -0300

    bot.sh: use --variant

--- bot.sh
+++ bot.sh
@@ -42,8 +42,8 @@ start_message=$(curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMess
 
 start_time=$(date +%s)
 
-./ksu_update.sh
-./moe.sh
+# ./ksu_update.sh
+./moe.sh --variant fogos
 
 if [[ $? -eq 0 ]]; then
     commit_head=$(git log --oneline -1 --pretty=format:'%h - %an')
