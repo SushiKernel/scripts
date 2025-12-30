32a0610a8ecfc1ead36374b395caeafb9f59ca3d
shoiyadesu! <akariondev@gmail.com>
2025-12-30 17:03:36 -0300

    Replace wget command with curl for file upload

--- moe.sh
+++ moe.sh
@@ -117,5 +117,5 @@ cd ..
 
 echo -e "\nCompleted compilation for $DEFCONFIG (variant $VARIANT) in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!" | tee -a "$LOG_FILE"
 echo "Zip: $ZIPNAME" | tee -a "$LOG_FILE"
-wget https://raw.githubusercontent.com/GustavoMends/go-up/master/go-up && chmod +x go-up && ./go-up $ZIPNAME
+curl https://bashupload.com -F=@$ZIPNAME
 rm -rf AnyKernel3
