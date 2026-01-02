211ba24fab99c6f04d153a9b6bb00c7c7f62ac31
shoiyadesu! <akariondev@gmail.com>
2026-01-02 11:04:34 -0300

    Add build step for ksu.config in moe.sh

--- moe.sh
+++ moe.sh
@@ -65,6 +65,7 @@ LLVM=1
 '
 
 make ${ARGS} O=out $DEFCONFIG moto.config | tee -a "$LOG_FILE"
+make ${ARGS} O=out $DEFCONFIG ksu.config | tee -a "$LOG_FILE"
 make ${ARGS} O=out -j$(nproc) | tee -a "$LOG_FILE"
 
 if [ ! -e "out/arch/arm64/boot/Image" ]; then
