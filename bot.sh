70f1c25cbdf6241aed70c899f026d787ef43e7ee
itzAkari <akariondev@gmail.com>
2026-09-17 12:19:13 -0300

    Modify sushi.sh command in bot.sh
    Update sushi.sh execution to include build flags.

--- bot.sh
+++ bot.sh
@@ -43,7 +43,7 @@ start_message=$(curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMess
 
 start_time=$(date +%s)
 
-./sushi.sh -v fogos > build_log.txt 2>&1
+BUILD=1 ANYKERNEL=1 ./sushi.sh
 
 if [[ $? -eq 0 ]]; then
     commit_head=$(git log --oneline -1 --pretty=format:'%h - %an')
